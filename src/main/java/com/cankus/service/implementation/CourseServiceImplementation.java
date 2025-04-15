95e5b1f98871569a05812fc23c88667eb4c18a1e
Ftosun21 <tosunfatih9.2@gmail.com>
2025-04-16 01:57:57 +0300

    added: us 5 - courseRepository extends JpaRepository , CourseMapper convertToEntity() & convertToDto() , CourseService findAll(),CourseServiceImlementation- courseRepository and courseMapper object and allArgsConstructor & findAll()

--- src/main/java/com/cankus/service/implementation/CourseServiceImplementation.java
+++ src/main/java/com/cankus/service/implementation/CourseServiceImplementation.java
@@ -1,4 +1,28 @@
 package com.cankus.service.implementation;
 
-public class CourseServiceImplementation {
+import com.cankus.dto.CourseDto;
+import com.cankus.mapper.CourseMapper;
+import com.cankus.repository.CourseRepository;
+import com.cankus.service.CourseService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CourseServiceImplementation implements CourseService {
+
+    private final CourseRepository courseRepository;
+    private final CourseMapper courseMapper;
+
+    public CourseServiceImplementation(CourseRepository courseRepository, CourseMapper courseMapper) {
+        this.courseRepository = courseRepository;
+        this.courseMapper = courseMapper;
+    }
+
+    @Override
+    public List<CourseDto> findAll() {
+        return courseRepository.findAll().stream()
+                .map(courseMapper::convertToDto).collect(Collectors.toList());
+    }
 }
