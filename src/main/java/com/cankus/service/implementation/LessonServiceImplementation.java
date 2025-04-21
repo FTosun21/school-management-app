83aa4227a45ff769ec830be792d7b395f9ae0bc5
Ftosun21 <tosunfatih9.2@gmail.com>
2025-04-21 21:55:23 +0300

    feat: US-9 - Lesson management initial implementation
    1. Entity - Course:
       - Removed unused imports.
    
    2. DTO - LessonDto:
       - Added fields (id, name, description, course, instructor).
       - Constructors, getters, and setters implemented.
    
    3. Mapper - LessonMapper:
       - Created methods to convert between Lesson and LessonDto.
    
    4. Service - LessonService & LessonServiceImpl:
       - Implemented findAll() method to retrieve all lessons.

---
+++ src/main/java/com/cankus/service/implementation/LessonServiceImplementation.java
@@ -0,0 +1,29 @@
+package com.cankus.service.implementation;
+
+import com.cankus.dto.LessonDto;
+import com.cankus.mapper.LessonMapper;
+import com.cankus.repository.LessonRepository;
+import com.cankus.service.LessonService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LessonServiceImplementation implements LessonService {
+
+    private final LessonRepository lessonRepository;
+    private final LessonMapper lessonMapper;
+
+    public LessonServiceImplementation(LessonRepository lessonRepository, LessonMapper lessonMapper) {
+        this.lessonRepository = lessonRepository;
+        this.lessonMapper = lessonMapper;
+    }
+
+
+    @Override
+    public List<LessonDto> findAll() {
+        return lessonRepository.findAll()
+                .stream()
+                .map(lessonMapper::convertToDto)
+                .collect(Collectors.toList());
+    }
+}
