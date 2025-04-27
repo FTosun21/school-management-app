e91ef24806e754eccf277576b689b1a072bd6a05
Ftosun21 <tosunfatih9.2@gmail.com>
2025-04-27 03:49:33 +0300

    US-14 - Manager can create new student feat:
    dto/StudentDto -> validation restrictions are added to StudentDto
    
    service/StudentService -> save() method is declared for creating new students
    
    service/impl/StudentServiceImpl -> save() method is implemented for persisting new students
    
    controller/StudentController -> createStudent() method is added to handle POST requests for creating students

--- src/main/java/com/cankus/controller/StudentController.java
+++ src/main/java/com/cankus/controller/StudentController.java
@@ -3,9 +3,13 @@ package com.cankus.controller;
 import com.cankus.dto.StudentDto;
 import com.cankus.enums.State;
 import com.cankus.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,4 +28,18 @@ public class StudentController {
         model.addAttribute("students",studentService.findAll());
         return "student/student-create";
     }
+
+    @PostMapping("/create")
+    public String createStudent(@Valid @ModelAttribute("student")StudentDto student,
+                                BindingResult bindingResult,
+                                Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("states",State.values());
+            model.addAttribute("students",studentService.findAll());
+            return "student/student-create";
+        }
+        studentService.save(student);
+        return "redirect:/student/create";
+    }
+
 }
--- src/main/java/com/cankus/dto/StudentDto.java
+++ src/main/java/com/cankus/dto/StudentDto.java
@@ -3,19 +3,33 @@ package com.cankus.dto;
 import com.cankus.entity.Address;
 import com.cankus.enums.Gender;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 
 public class StudentDto {
 
     private Long id;
 
+    @NotBlank(message = "First Name is a required field")
+    @Size(max = 15, min = 2, message = "First Name must be between 2 and 15 characters long")
+    @Pattern(regexp = "[A-Z]\\w*", message = "First Name must starts with Uppercase character")
     private String firstName;
 
+    @NotBlank(message = "Last Name is a required field")
+    @Size(max = 15, min = 2, message = "Last Name must be between 2 and 15 characters long")
+    @Pattern(regexp = "[A-Z]\\w*", message = "Last Name must starts with Uppercase character")
     private String lastName;
 
+    @Email(message = "Email is a required field")
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
+            message = "Provide a suitable email address ex: michael23jordan23@test.com")
     private String email;
 
+    @NotNull(message = "Please select a Gender")
     private Gender gender;
 
+    @Valid
+    @NotNull
     private Address address;
 
     public StudentDto() {
